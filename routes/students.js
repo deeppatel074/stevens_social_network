1984821f4d466e28326d346ea03843944311f10b
deeppatel074 <deeppatel074@gmail.com>
2022-04-20 16:17:58 -0400

    Added multers

--- routes/students.js
+++ routes/students.js
@@ -2,179 +2,179 @@ const express = require('express');
 const router = express.Router();
 const valid = require("../tasks/valid")
 const studentData = require("../data/students");
-const axios = require ("axios");
-// const path = require("path");
-
+const upload = require('../config/multer')
 
 router
-.route('/')
-.get(async (req, res) => {
-    try{
-        if (req.session.user){
-            res.redirect("/private");
+    .route('/')
+    .get(async (req, res) => {
+        try {
+            if (req.session.user) {
+                res.redirect("/private");
+                return;
+            }
+            else {
+                res.render("students/login", {
+                    title: "Log In"
+                });
+                return;
+            }
+        } catch (e) {
+            res.status(500).json({ Error: e })
             return;
         }
-        else{
-            res.render("students/login",{
-                title: "Log In"
-            });
-            return;
-        }
-    }catch(e){
-        res.status(500).json({Error: e})
-        return;
-    }
-  });
+    });
 
-  //Log in
+//Log in
 
-  router
-  .route("/login")
-    .post(async (req,res) =>{
-        let {email,password} = req.body;
-        try{
+router
+    .route("/login")
+    .post(async (req, res) => {
+        let { email, password } = req.body;
+        try {
             email = await valid.validateEmail(email);
             await valid.validatePassword(password);
-        }catch(e){
-            res.status(400).render("students/login",{
-                title : "Errors",
-                hasErrors : true,
-                errors : e,
-                email : email,
-                password :password
+        } catch (e) {
+            res.status(400).render("students/login", {
+                title: "Errors",
+                hasErrors: true,
+                errors: e,
+                email: email,
+                password: password
             });
             return;
         }
-        
-        try{
-            let student = await studentData.checkStudent(email,password);
-            if(student.authenticated == true){
-              req.session.user = {email:email};
-              res.redirect('/private');
-              return;
+
+        try {
+            let student = await studentData.checkStudent(email, password);
+            if (student.authenticated == true) {
+                req.session.user = { email: email };
+                res.redirect('/private');
+                return;
             }
-            if(student.authenticated ==false && !e){
+            if (student.authenticated == false && !e) {
                 res.status(500).json(
-                    {Errors : "Internal Server Error"}
+                    { Errors: "Internal Server Error" }
                 );
                 return;
-                
+
             }
-        }catch(e){
-            res.status(400).render("students/login",{
-                title : "Errors",
-                hasErrors : true,
-                errors : e
+        } catch (e) {
+            res.status(400).render("students/login", {
+                title: "Errors",
+                hasErrors: true,
+                errors: e
             });
             return;
         }
     });
 
-    //Sign Up
+//Sign Up
 
-    router
+router
     .route("/signup")
-    .get(async (req,res) => {
-        try{
-            if(!req.session.user){
-                res.status(200).render("students/signup",{
-                    title : "Sign up"
+    .get(async (req, res) => {
+        try {
+            if (!req.session.user) {
+                res.status(200).render("students/signup", {
+                    title: "Sign up"
                 });
                 return;
-            }else{
+            } else {
                 res.status(200).redirect("/private");
-            return;
+                return;
             }
-            
-        }catch(e){
-            res.status(500).json({error : e});
+
+        } catch (e) {
+            res.status(500).json({ error: e });
             return;
         }
     })
-    
-    .post(async (req,res) =>{
+
+    .post(upload.single("profileUrl"), async (req, res) => {
         const studentPostData = req.body;
-        // console.log(studentPostData);
-        try{
-            studentPostData.firstName = await valid.checkString( studentPostData.firstName,"firstName");
-            studentPostData.lastName = await valid.checkString( studentPostData.lastName,"lastName");
-            await valid.validatePhoneNumber( studentPostData.phoneNumber);
-            studentPostData.email = await valid.validateEmail( studentPostData.email);
-            await valid.validatePassword( studentPostData.password);
+        console.log(req.file);
+        let profilePic = req.file.originalname;
+
+        try {
+            studentPostData.firstName = await valid.checkString(studentPostData.firstName, "firstName");
+            studentPostData.lastName = await valid.checkString(studentPostData.lastName, "lastName");
+            await valid.validatePhoneNumber(studentPostData.phoneNumber);
+            studentPostData.email = await valid.validateEmail(studentPostData.email);
+            await valid.validatePassword(studentPostData.password);
             await valid.validateConfirmPassword(studentPostData.password, studentPostData.confirmPassword);
-        }catch(e){
-            res.status(400).render("students/signup",{
-                title : "Errors",
-                hasErrorsSign : true,
-                errors : e,
-                studentPostData : studentPostData
+        } catch (e) {
+            res.status(400).render("students/signup", {
+                title: "Errors",
+                hasErrorsSign: true,
+                errors: e,
+                studentPostData: studentPostData
             });
             return;
         }
-        
-        try{
+
+        try {
             // const {firstName, lastName,email,password,phoneNumber,profileUrl} =  studentPostData
-            let student = await studentData.createStudent(studentPostData.firstName,studentPostData.lastName,studentPostData.email,studentPostData.password,studentPostData.phoneNumber,studentPostData.profileUrl);
-            if(student.studentInserted == true){
-              res.redirect('/');
-              return;
-            }if(student.studentInserted == false && !e){
-                res.status(500).json({Error: "Internal Server Error"});
+            let student = await studentData.createStudent(studentPostData.firstName, studentPostData.lastName, studentPostData.email, studentPostData.password, studentPostData.phoneNumber, profilePic);
+            if (student.studentInserted == true) {
+                res.redirect('/');
+                return;
+            } if (student.studentInserted == false && !e) {
+                res.status(500).json({ Error: "Internal Server Error" });
                 return;
             }
-        }catch(e){
-            res.status(400).render("students/signup",{
-                title : "Errors",
-                hasErrorsSign : true,
-                errors : e,
-                studentPostData : studentPostData
+        } catch (e) {
+            res.status(400).render("students/signup", {
+                title: "Errors",
+                hasErrorsSign: true,
+                errors: e,
+                studentPostData: studentPostData
             });
             return;
         }
     });
 
-    //private 
+//Redirect to home page 
 
-    router 
+router
     .route("/private")
-    .get(async (req,res) => {
-        if(req.session.user){
+    .get(async (req, res) => {
+        if (req.session.user) {
             // let profile = await studentData.getProfileUrl(req.session.user.email);
-            res.status(200).render("students/private",{
+            res.status(200).render("students/private", {
                 email: req.session.user.email,
-                title : "Private",
-                
+                title: "Private",
+
             })
             return;
         }
     })
 
 
-    //Logout
+//Logout
 
-    router
+router
     .route("/logout")
-    .get(async (req,res) =>{
+    .get(async (req, res) => {
         if (req.session) {
             req.session.destroy(err => {
-              if (err) {
-                res.status(400).send('Unable to log out')
-                return;
-              } else {
-                res.render("students/logout", {
-                    title : "Logout"
-                });
-                return;
+                if (err) {
+                    res.status(400).send('Unable to log out')
+                    return;
+                } else {
+                    res.render("students/logout", {
+                        title: "Logout"
+                    });
+                    return;
 
-              }
+                }
             });
-          } else {
+        } else {
             res.end()
             return;
-          }
+        }
     })
 
-    module.exports = router;
+module.exports = router;
 
 
 
