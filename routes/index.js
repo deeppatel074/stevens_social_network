1f63d7e718d5fe3a834c0bee18c0debef39bf5a8
VyaqtiVikasSingh <100976548+VyaqtiVikasSingh@users.noreply.github.com>
2022-04-20 16:07:16 -0400

    Login & Signup UI and routes basic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+const indexRoute = require('../routes/pages.js');
+
 const constructorMethod = (app) => {
 
-
+    app.use('/', indexRoute);
     app.use('*', (req, res) => {
         res.status(404).json({ error: "Not Found!!" });
     });
