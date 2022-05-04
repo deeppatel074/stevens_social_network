931f2c126183882f539d2fd38d214e5b4ee3dd22
deeppatel074 <deeppatel074@gmail.com>
2022-05-04 12:33:47 -0400

    Events

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,8 @@ const valid = require("../data/valid");
 const upload = require('../config/multer');
 const xss = require('xss');
 
+
+// will render events list
 router.get('/', async (req, res) => {
     if (req.session.user) {
         return res.status(200).render("events/eventsList", {
@@ -14,5 +16,32 @@ router.get('/', async (req, res) => {
         return res.redirect('/');
     }
 });
+
+
+// will render events Create page
+router.get('/create', async (req, res) => {
+    // if (req.session.user) {
+    return res.status(200).render("events/eventsCreate", {
+        title: "Create Events",
+        logged: true
+    });
+    // } else {
+    //     return res.redirect('/');
+    // }
+});
+
+// will render events detail page
+router.get('/:id', async (req, res) => {
+    // if (req.session.user) {
+    return res.status(200).render("events/eventsDetail", {
+        title: "Events",
+        logged: true
+    });
+    // } else {
+    //     return res.redirect('/');
+    // }
+});
+
+
 
 module.exports = router;
